26adbf1cbdaa720b5f37db36e6c086262e0c32f6
Jrandle10 <91491549+Jrandle10@users.noreply.github.com>
2021-11-08 13:51:35 -0700

    write index function for profiles model

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,7 +1,17 @@
 import { Profile } from '../models/profile.js'
 
 function index(req, res) {
-  
+  Profile.find({})
+  .then(profiles => {
+    res.render('profiles/index', {
+      profiles,
+      title: '👤'
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect(`/profiles/${req.user.profile._id}`)
+  })
 }
 
 export {
