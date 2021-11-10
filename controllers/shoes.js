2c9cee64eb6c0b56c2040ccb0d003a5f2a589e1c
Jrandle10 <91491549+Jrandle10@users.noreply.github.com>
2021-11-10 01:21:37 -0700

    add edit and update to shoe page

--- controllers/shoes.js
+++ controllers/shoes.js
@@ -26,7 +26,58 @@ function create(req,res) {
   })
 }
 
+function show(req, res) {
+  Shoe.findById(req.params.id)
+  .populate("owner")
+  .then(shoe => {
+    res.render('shoes/show', {
+      shoe,
+      title: "👟 show"
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/shoes')
+  })
+}
+
+
+function edit(req, res) {
+  Shoe.findById(req.params.id)
+  .then(shoe => {
+    res.render('shoes/edit', {
+      shoe,
+      title: "edit 👟"
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/shoes')
+  })
+}
+
+function update(req, res) {
+  Shoe.findById(req.params.id)
+  .then(shoe => {
+    if (shoe.owner.equals(req.user.profile._id)) {
+      shoe.updateOne(req.body, {new: true})
+      .then(()=> {
+        res.redirect(`/shoes/${shoe._id}`)
+      })
+    } else {
+      throw new Error ('🚫 Not authorized 🚫')
+    }
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect(`/shoes`)
+  })
+}
+
 export {
   index,
   create,
+  show,
+  edit,
+  update,
 }
--- routes/shoes.js
+++ routes/shoes.js
@@ -6,8 +6,14 @@ const router = Router()
 
 //localhost:3000/shoes
 router.get('/', shoesCtrl.index)
+//localhost:3000/shoes/:id
+router.get('/:id', shoesCtrl.show)
+//localhost:3000/shoes/:id/edit
+router.get('/:id/edit', isLoggedIn, shoesCtrl.edit)
 //localhost:3000/shoes
 router.post('/', isLoggedIn, shoesCtrl.create)
+//localhost:3000/shoes/:id
+router.put('/:id', isLoggedIn, shoesCtrl.update)
 
 
 
